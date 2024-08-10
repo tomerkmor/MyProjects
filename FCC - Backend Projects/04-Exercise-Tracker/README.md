30b0455746fd63d1d364077d4f3c8884ff0bd417
tomerkmor <sexylooter@gmail.com>
2024-08-10 14:39:01 +0300

    context

--- FCC - Backend Projects/04-Exercise-Tracker/README.md
+++ FCC - Backend Projects/04-Exercise-Tracker/README.md
@@ -1,6 +1,7 @@
 # Exercise Tracker
 * Create user
-* Add Exercises(logs)
+* Get users
+* Add exercises(logs)
 * Read all the logs for selected username
 
 This is the boilerplate for the Exercise Tracker project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/exercise-tracker
--- FCC - Backend Projects/05-File-Metadata-MicroService/index.js
+++ FCC - Backend Projects/05-File-Metadata-MicroService/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const mongoose = require('mongoose')
+const multer  = require('multer')
+const upload = multer({dest: 'uploads/'})
 
 var app = express();
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// Basic Configuration
+app.use(express.json())
+  // helps us to make sure we can access req.body
+app.use(express.urlencoded({extended: true})) 
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +20,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'),async (req,res) => {
+  console.log(req.file)
+  const enteredFile = await req.file
+  res.json({
+    name: enteredFile.originalname,
+    type: enteredFile.mimetype,
+    size: enteredFile.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
